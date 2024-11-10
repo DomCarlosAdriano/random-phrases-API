5701a32f1ae8cefda85c1d33e0947ba5e3b95cf1
DomCarlosAdriano <dogmalcarlos@gmail.com>
2024-11-09 21:49:37 -0300

    First commit

---
+++ src/main/java/br/com/alura/screenmatch_frases/Frase.java
@@ -0,0 +1,60 @@
+package br.com.alura.screenmatch_frases;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "frases")
+public class Frase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String frase;
+
+    private String personagem;
+
+    private String poster;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitula() {
+        return titulo;
+    }
+
+    public void setTitula(String titula) {
+        this.titulo = titula;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+
+    public String getPersonagem() {
+        return personagem;
+    }
+
+    public void setPersonagem(String personagem) {
+        this.personagem = personagem;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+}
